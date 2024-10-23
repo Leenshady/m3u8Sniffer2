f4e16528c0e72db488e09f0822c42e6c290741fc
shady <l.vvv@foxmail.com>
2024-10-23 16:45:47 +0800

    修复m3u8链接跨页面不清除问题

--- js/background.js
+++ js/background.js
@@ -1,13 +1,13 @@
-var msg = {}
-msg.m3u8list = []
+m3u8list = []
 var pattern = /http[s]?[://]{1}[-A-Za-z0-9+&@#/%?=~_|!:,.;]*[-A-Za-z0-9+&@#/%=~_|]*.m3u8$/
 var url = ""
+//chrome.storage.session.setAccessLevel({accessLevel:'TRUSTED_AND_UNTRUSTED_CONTEXTS'})
 
 //监听页面网络请求
 chrome.webRequest.onBeforeRequest.addListener(details => {
     chrome.tabs.query({ active: true }, tabs => {
         if (url != tabs[0].url) {
-            msg.m3u8list = []
+            m3u8list = []
         }
         url = tabs[0].url
         var tmp
@@ -17,10 +17,21 @@ chrome.webRequest.onBeforeRequest.addListener(details => {
             tmp = details.url.trim()
         }
         if (pattern.test(tmp)) {
-            if (msg.m3u8list.indexOf(details.url)==-1) {
-                msg.m3u8list.push(details.url)
-                chrome.storage.session.set({'data':msg.m3u8list})
+            if (m3u8list.indexOf(details.url)==-1) {
+                m3u8list.push(details.url)
+                getCurrentTab().then((tab)=>{
+                    var tabId = ("tab"+tab.id).toString()
+                    chrome.storage.session.set({[tabId]:m3u8list})
+                })
+                
             }
         }
     });
 }, { urls: ["<all_urls>"] }, ["extraHeaders"]);
+
+//获取当前标签页
+async function getCurrentTab() {
+    let queryOptions = { active: true, currentWindow: true };
+    let [tab] = await chrome.tabs.query(queryOptions);
+    return tab;
+  }
--- js/popup.js
+++ js/popup.js
@@ -1,5 +1,5 @@
 var m3u8list = [];
-chrome.storage.session.setAccessLevel({accessLevel:'TRUSTED_AND_UNTRUSTED_CONTEXTS'})
+//chrome.storage.session.setAccessLevel({accessLevel:'TRUSTED_AND_UNTRUSTED_CONTEXTS'})
 var language = (navigator.language || navigator.userLanguage).toLowerCase();
 var i18n_text={}
 
@@ -9,10 +9,12 @@ $(function(){
         i18n_text.ext_name="m3u8嗅探器2"
         i18n_text.copy_msg="复制成功！"
         i18n_text.copy_btn="复制"
+        i18n_text.nothing_msg="空空如也！<br>请尝试刷新页面！"
     }else{
         i18n_text.ext_name="m3u8Sniffer2"
         i18n_text.copy_msg="Copy Success!"
         i18n_text.copy_btn="Copy"
+        i18n_text.nothing_msg="Find nothing!<br>Please try refreshing!"
     }
     $("#ext-name").text(i18n_text.ext_name)
     $("#msg").text(i18n_text.copy_msg)
@@ -23,12 +25,17 @@ $(function(){
 })
 
 //获取session内的存储数据
-function getData(){
-    chrome.storage.session.get('data').then((value)=>{
-        value['data'].forEach(element => {
-            m3u8list.push(element)
-        });
-        render(m3u8list)
+async function getData(){
+    getCurrentTab().then((tab)=>{
+        let tabId = "tab"+tab.id
+        chrome.storage.session.get([tabId]).then((value)=>{
+            if(!isEmptyObject(value)){
+                value[tabId].forEach(element => {
+                    m3u8list.push(element)
+                });
+            }
+            render(m3u8list)
+        })
     })
 }
 
@@ -36,19 +43,23 @@ function getData(){
 function render(list){
     if(list !== undefined){
         if(list.length==0){
-            $(".alert-danger").addClass("show")
-            $(".alert-danger").removeAttr("hidden")
+            $("#box").append('<div style="mt-1 mb-1"><span style="max-width: 200px;white-space: nowrap;display: inline-block;overflow: hidden;text-overflow: ellipsis;line-height: 1.5;">'+i18n_text.nothing_msg+'</span></div>')
         }else{
             $("#box").html()
             for (i = 0; i < list.length; i++) {
                 $("#box").append('<div id="url' + i + '" style="mt-1 mb-1"><span style="max-width: 200px;white-space: nowrap;display: inline-block;overflow: hidden;text-overflow: ellipsis;line-height: 1.5;">' + (list[i].indexOf("?")!=-1?list[i].slice(0,list[i].indexOf("?")).slice(list[i].lastIndexOf("/")+1,list[i].length):list[i].slice(list[i].lastIndexOf("/")+1,list[i].length)) + '</span><a href="#" style="float: right;">'+i18n_text.copy_btn+'</a></div>');
                 $("#url" + i).click({ "url": list[i] }, copyUrl);
             }
-            $(".alert-danger").removeClass("show")
-            $(".alert-danger").attr("hidden","hidden")
         }
     }
 }
+
+//获取当前标签页
+async function getCurrentTab() {
+    let queryOptions = { active: true, currentWindow: true };
+    let [tab] = await chrome.tabs.query(queryOptions);
+    return tab;
+  }
 
 //复制链接到剪切板
 function copyUrl(obj) {
@@ -61,5 +72,9 @@ function copyUrl(obj) {
     },2000);//2秒后消失
 }
 
+//对象判空函数
+function isEmptyObject(obj) {
+    return JSON.stringify(obj) === '{}';
+}
 
 
